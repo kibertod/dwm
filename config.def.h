d5f2f88cbfce96f90e2876e0eef9c83b0f084849
kibertod <cyber704d@gmail.com>
2025-04-12 05:10:39 +0300

    saas

--- config.def.h
+++ config.def.h
@@ -5,6 +5,10 @@
 static const unsigned int gappx = 10;
 static const unsigned int borderpx = 2; /* border pixel of windows */
 static const unsigned int snap = 32; /* snap pixel */
+static const int usealtbar = 1; /* 1 means use non-dwm status bar */
+static const char* altbarclass = "Polybar"; /* Alternate bar class name */
+static const char* altbarcmd =
+    "bash -c \"/usr/bin/polybar -r bar &\""; /* Alternate bar launch command */
 static const int showbar = 1; /* 0 means no bar */
 static const int topbar = 1; /* 0 means bottom bar */
 static const char* fonts[] = { "CommitMono Nerd Font Mono:size=10" };
